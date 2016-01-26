c53edcd5decfae0183a88367fb22590dd5056c4d
Daniel Marell <daniel.marell@gmail.com>
2016-01-26 14:36:50 +0100

    Debug print.

--- opt/scripts/publish_docker.sh
+++ opt/scripts/publish_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Deploy and run smoketest on doodle
+# Deploy and run smoketest on app
 if [ "$#" -ne 1 ]; then
     echo "Illegal number of parameters, imagename as arg"
 fi
@@ -8,5 +8,12 @@ IMAGE_NAME=$1
 
 VERSION=`grep AppVersion BuildInfo.properties | awk -F'=' '{print $2}'`
 TUTUMUSER=`grep -e 'user =' ~/.tutum  | awk '{print $3}'`
+
+# TODO remove debug print
+echo In publish_docker.sh:
+echo TUTUMUSER: $TUTUMUSER
+echo IMAGE_NAME: IMAGE_NAME
+echo VERSION: VERSION
+
 docker tag ${IMAGE_NAME}:${VERSION} tutum.co/${TUTUMUSER}/${IMAGE_NAME}:${VERSION} 
 docker push tutum.co/${TUTUMUSER}/${IMAGE_NAME}:${VERSION}
